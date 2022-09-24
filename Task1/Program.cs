ba563def68c041916c9504ba1236574f1f53a684
Roman <roman_strashko.8@mail.ru>
2022-09-24 20:09:50 +0300

    Решение задачи 1 изминения

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,10 +4,10 @@ Console.Write("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine());
 int index = number / 10000;
 int index1 = ((number % 10000) / 1000);
-int index2 = ((number % 100) % 10);
+int index2 = ((number % 100) / 10);
 int index3 = (number % 10);
 
-if(index == index3 || index1 == index2)
+if(index == index3 && index1 == index2)
 {
     Console.WriteLine($"Число {number} палиндром");
 } 
